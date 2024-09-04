1bbe4b62ff03f7cf56d29fed940012118f8830d8
Arewa100 <olasorexway@gmail.com>
2024-09-04 19:10:07 +0100

    practice

---
+++ practice.js
@@ -0,0 +1,15 @@
+let firstNumber = 6;
+let secondNumber = 4;
+let result = (firstNumber + secondNumber)
+
+console.log(`the result is ${result}`)
+console.log(true || false)
+
+
+let name = null;
+console.log(typeof name)
+console.log("4" - 1)
+console.log(typeof(null))
+
+let location = "";
+console.log(location && true)
